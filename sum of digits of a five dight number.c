80de2ff03076dbc141a84a82e37d02ce378ee760
jhansisharunyalakkoju-cmyk <jhansisharunyalakkoju@gmail.com>
2026-02-28 12:17:52 +0530

    Create sum of digits of a five dight number.c

---
+++ sum of digits of a five dight number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+
+    sum += n % 10;   // last digit
+    n /= 10;
+
+    sum += n % 10;
+    n /= 10;
+
+    sum += n % 10;
+    n /= 10;
+
+    sum += n % 10;
+    n /= 10;
+
+    sum += n % 10;
+
+    printf("%d", sum);
+
+    return 0;
+}
