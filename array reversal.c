aa21e61bc710bbdd3a1069afe4361b58143195ce
jhansisharunyalakkoju-cmyk <jhansisharunyalakkoju@gmail.com>
2026-02-28 12:28:28 +0530

    array reversal.c

---
+++ array reversal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int *arr = (int*)malloc(n * sizeof(int));
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
